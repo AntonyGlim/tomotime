b4e1d384e464ef0148fdb8fa43cc151be4347a41
Antony.Glim <Antony.Glim@gmail.com>
2019-10-19 20:14:08 +0300

    Initial commit

---
+++ src/main/java/glim/antony/tomotime/TomotimeApplication.java
@@ -0,0 +1,13 @@
+package glim.antony.tomotime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomotimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomotimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/glim/antony/tomotime/TomotimeApplicationTests.java
@@ -0,0 +1,13 @@
+package glim.antony.tomotime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomotimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
