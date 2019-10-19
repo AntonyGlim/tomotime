f5066b28533dfbdef993550e2a74ca8daf973048
Antony.Glim <Antony.Glim@gmail.com>
2019-10-19 20:15:20 +0300

    Add min needed count of files for first start

---
+++ src/main/resources/db/migration/V1__initial_database.sql
@@ -0,0 +1,11 @@
+-- *for Windows
+-- \! chcp 1251
+
+-- *for Linux
+-- sudo -i -u postgres
+-- psql
+
+-- DROP SCHEMA IF EXISTS tomotime CASCADE;
+-- CREATE SCHEMA tomotime;
+--
+-- SET search_path TO tomotime;
