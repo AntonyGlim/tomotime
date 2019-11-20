f6c9464f281e64b7785a8efb8b003deb0a9b9084
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-11-20 20:17:58 +0300

    Add class User and Role for security

---
+++ src/main/java/glim/antony/tomotime/entities/User.java
@@ -0,0 +1,52 @@
+package glim.antony.tomotime.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> orders;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
+
+    public User(String phone, String password, String firstName, String lastName, String email) {
+        this.phone = phone;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
